be134a46324ce791c32a11a5c89c367b5292f39b
Yorch2020 <george.barrerab@gmail.com>
2021-01-06 21:43:15 -0600

    05-jan-2020

--- assets/js/operador-ternario.js
+++ assets/js/operador-ternario.js
@@ -6,22 +6,38 @@
  * Entra a un sitio web, para consultar si esta abierto hoy...
  */
 
-const dia =0; // 0: domingo , 1: Lunes
+const dia = 1; // 0: domingo , 1: Lunes
 const horaActual = 10;
 
 let horaApertura;
 let mensaje; // Esta abierto, esta cerrado, hoy abrimos a las XX
 
-if (dia === 0 || dia ===6){
-    console.log ('Fin de semana');
-    horaApertura = 9;
+// if (dia === 0 || dia ===6){
+
+//if ([0,6].includes(dia)) {
+
+   // console.log ('Fin de semana');
+   // horaApertura = 9;
+   // } else { 
+    //    console.log ('Dia de semana');
+    //    horaApertura = 11;
+
+
+  //  } 
+
+  horaApertura = ([0,6].includes (dia)) ? 9 :11;
+
+
+    if (horaActual>= horaApertura){
+        mensaje = 'Esta abierto';
+
     } else {
-        console.log ('Dia de semana');
-        horaApertura = 11;
+
+        mensaje = `Esta cerrado, hoy abrimos a las ${horaApertura}`;
+    }
+
+    mensaje = (horaActual >= horaApertura) ? 'Esta abierto': `ESta cerrado, hoy abrimos a las ${horaApertura} `;
 
 
-    } 
-
-
-    console.log ({horaApertura});
+    console.log ({horaApertura, mensaje});
 
