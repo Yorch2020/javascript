20442c8b0c6408dcb5b68940613fbb7abc2a26f0
Yorch2020 <george.barrerab@gmail.com>
2021-01-06 22:33:14 -0600

    06-jan-2020

---
+++ assets/js/ciclos/while-do-while.js
@@ -0,0 +1,18 @@
+const carros = ['Ford', 'Mazda','Honda','Toyota'];
+
+let i = 0;
+
+while (i < carros.length) {
+
+    if (i===2) {
+
+        break;
+    }
+ 
+    console.log (carros[i]);
+    i++;
+}
+
+// undefined
+// null 
+// false
--- assets/js/protip-ternario.js
+++
@@ -1,5 +0,0 @@
-const elMayor = (a, b) => (a > b) ? a : b;
-
-
-
-console.log (elMayor(5, 15));
